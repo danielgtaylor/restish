ae91e9f5eb4c338607ff715c235b15d0d0078e8b
Daniel G. Taylor <danielgtaylor@gmail.com>
2020-05-17 23:56:02 -0700

    test: add basic get test

---
+++ cli/cli_test.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func run(cmd string) string {
+	viper.Set("nocolor", true)
+	Init("test")
+	AddContentType("application/json", 1, JSON{})
+
+	capture := &strings.Builder{}
+	Stdout = capture
+	Root.ParseFlags(strings.Split(cmd, " "))
+	args := Root.Flags().Args()
+	Root.Run(Root, args)
+
+	return capture.String()
+}
+
+func expectJSON(t *testing.T, cmd string, expected interface{}) {
+	captured := run("-o json " + cmd)
+
+	var actual map[string]interface{}
+	err := json.Unmarshal([]byte(captured), &actual)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual["body"])
+}
+
+func TestGetURI(t *testing.T) {
+	defer gock.Off()
+
+	// TODO: set up gock, make a request
+	gock.New("http://example.com").Get("/foo").Reply(200).JSON(map[string]interface{}{
+		"Hello": "World",
+	})
+
+	expectJSON(t, "http://example.com/foo", map[string]interface{}{
+		"Hello": "World",
+	})
+}

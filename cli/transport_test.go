2c33b07e986e94810039d23f30a01f91b992c96c
Daniel G. Taylor <danielgtaylor@gmail.com>
2020-06-29 14:50:39 -0700

    refactor: cache logic to utility function

--- cli/transport_test.go
+++ cli/transport_test.go
@@ -14,6 +14,7 @@ func TestMinCachedTransport(t *testing.T) {
 
 	gock.New("http://example.com").Get("/success").Reply(200)
 	gock.New("http://example.com").Get("/cached").Reply(200).SetHeader("cache-control", "max-age=10")
+	gock.New("http://example.com").Get("/expires").Reply(200).SetHeader("expires", "Sun, 1 Jan 2020 12:00:00 GMT")
 	gock.New("http://example.com").Get("/modify").Reply(200).SetHeader("cache-control", "public")
 	gock.New("http://example.com").Get("/error").Reply(400)
 
@@ -34,6 +35,15 @@ func TestMinCachedTransport(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, resp.StatusCode, 200)
 	assert.Equal(t, resp.Header.Get("cache-control"), "max-age=10")
+
+	// Already-cached requests should not be touched.
+	req, _ = http.NewRequest(http.MethodGet, "http://example.com/expires", nil)
+	resp, err = tx.RoundTrip(req)
+
+	assert.NoError(t, err)
+	assert.Equal(t, resp.StatusCode, 200)
+	assert.NotEmpty(t, resp.Header.Get("expires"))
+	assert.Equal(t, resp.Header.Get("cache-control"), "")
 
 	// Already-set header should be modified instead of replaced.
 	req, _ = http.NewRequest(http.MethodGet, "http://example.com/modify", nil)
